781019763c206a92afbfd102edacb8c97c57abc1
mp487 <martin.peters4@uni-rostock.de>
2013-11-14 16:13:41 +0100

    - splitted Dataholder Model into Model and ModelResult

---
+++ morre.client/src/main/java/de/unirostock/sems/morre/client/dataholder/Model.java
@@ -0,0 +1,75 @@
+package de.unirostock.sems.morre.client.dataholder;
+
+public class Model {
+
+	protected String modelName;
+	protected String modelID;
+	protected long databaseID;
+	protected String documentURI;
+	protected String filename;
+	
+	
+	public Model(String modelName, String modelID, long databaseID, String documentURI, String filename) {
+		super();
+		this.modelName = modelName;
+		this.modelID = modelID;
+		this.databaseID = databaseID;
+		this.documentURI = documentURI;
+		this.filename = filename;
+	}
+
+
+	public String getModelName() {
+		return modelName;
+	}
+
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public String getModelID() {
+		return modelID;
+	}
+
+
+	public void setModelID(String modelId) {
+		this.modelID = modelId;
+	}
+
+
+	public long getDatabaseID() {
+		return databaseID;
+	}
+
+
+	public void setDatabaseID(long databaseId) {
+		this.databaseID = databaseId;
+	}
+
+
+	public String getDocumentURI() {
+		return documentURI;
+	}
+
+
+	public void setDocumentURI(String documentUri) {
+		this.documentURI = documentUri;
+	}
+
+
+	public String getFilename() {
+		return filename;
+	}
+
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	@Override
+	public String toString() {
+		return "ModelResult [modelName=" + modelName + ", modelId=" + modelID + "]";
+	}
+
+}
--- morre.client/src/main/java/de/unirostock/sems/morre/client/dataholder/ModelResult.java
+++ morre.client/src/main/java/de/unirostock/sems/morre/client/dataholder/ModelResult.java
@@ -1,88 +1,25 @@
 package de.unirostock.sems.morre.client.dataholder;
 
-public class ModelResult {
+public class ModelResult extends Model {
 
-	private String modelName;
 	private float score;
-	private String modelID;
-	private long databaseID;
-	private String documentURI;
-	private String filename;
-	
 	
 	public ModelResult(String modelName, float score, String modelID, long databaseID, String documentURI, String filename) {
-		super();
-		this.modelName = modelName;
+		super(modelName, modelID, databaseID, documentURI, filename);
 		this.score = score;
-		this.modelID = modelID;
-		this.databaseID = databaseID;
-		this.documentURI = documentURI;
-		this.filename = filename;
 	}
-
-
-	public String getModelName() {
-		return modelName;
-	}
-
-
-	public void setModelName(String modelName) {
-		this.modelName = modelName;
-	}
-
 
 	public float getScore() {
 		return score;
 	}
 
-
 	public void setScore(float score) {
 		this.score = score;
 	}
 
-
-	public String getModelID() {
-		return modelID;
-	}
-
-
-	public void setModelID(String modelId) {
-		this.modelID = modelId;
-	}
-
-
-	public long getDatabaseID() {
-		return databaseID;
-	}
-
-
-	public void setDatabaseID(long databaseId) {
-		this.databaseID = databaseId;
-	}
-
-
-	public String getDocumentURI() {
-		return documentURI;
-	}
-
-
-	public void setDocumentURI(String documentUri) {
-		this.documentURI = documentUri;
-	}
-
-
-	public String getFilename() {
-		return filename;
-	}
-
-
-	public void setFilename(String filename) {
-		this.filename = filename;
-	}
-
 	@Override
 	public String toString() {
-		return "ModelResult [modelName=" + modelName + ", modelId=" + modelID + "]";
+		return "ModelResult [modelName=" + modelName + ", modelId=" + modelID + ", score=" + score + "]";
 	}
 	
 }
