01398e11e0f834c4a2fb585f34660b2202d06aa6
Martin Peters <martin.peters4@uni-rostock.de>
2013-12-19 16:07:07 +0100

    - added Query for simple_sbml_model_query

--- morre.client/src/main/java/de/unirostock/sems/morre/client/QueryType.java
+++ morre.client/src/main/java/de/unirostock/sems/morre/client/QueryType.java
@@ -13,6 +13,7 @@ public class QueryType {
 	
 	public final static String CELLML_MODEL_QUERY = "cellml_model_query";
 	public final static String SIMPLE_CELLML_MODEL_QUERY = "simple_cellml_model_query";
+	public final static String SIMPLE_SBML_MODEL_QUERY = "simple_sbml_model_query";
 	public final static String PERSON_MODEL_QUERY = "person_model_query";
 	public final static String PUBLICATION_MODEL_QUERY = "publication_model_query";
 	public final static String ANNOTATION_MODEL_QUERY = "annotation_model_query";
